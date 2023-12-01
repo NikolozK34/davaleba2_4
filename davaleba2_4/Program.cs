c4fb66b8a2f477d171eb79e1839fd42bbb10520a
NikolozK34 <kekelidze97@gmail.com>
2023-12-01 17:04:50 +0400

    Add project files.

---
+++ davaleba2_4/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace davaleba2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("sheiyvane pirveli ricxvi: ");
+
+            int n1 = int.Parse(Console.ReadLine());
+
+            Console.Write("sheiyvane meore ricxvi: ");
+
+            int n2 = int.Parse(Console.ReadLine());
+
+            Console.Write("sheiyvane mesame ricxvi: ");
+
+            int n3 = int.Parse(Console.ReadLine());
+
+            if(n1 >= n2 && n1 >= n3)
+            {
+                if (n1 > n2 && n1 > n3)
+                {
+                    Console.WriteLine("pirveli ricxvi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n1 == n2)
+                {
+                    Console.WriteLine("pirveli da meore ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n1 == n3)
+                {
+                    Console.WriteLine("pirveli da mesame ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+                
+            }else if(n2 >= n1 && n2 >= n3)
+            {
+                if (n2 > n1 && n2 > n3)
+                {
+                    Console.WriteLine("meore ricxvi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n2 == n1)
+                {
+                    Console.WriteLine("meore da pirveli ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n2 == n3)
+                {
+                    Console.WriteLine("meore da mesame ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+            }
+            else if( n3 >= n1 && n3 >= n2)
+            {
+                if (n3 > n1 && n3 > n2)
+                {
+                    Console.WriteLine("mesame ricxvi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n3 == n1)
+                {
+                    Console.WriteLine("mesame da pirveli ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+
+                if (n3 == n2)
+                {
+                    Console.WriteLine("mesame da meore ricxvebi maqsimaluria warmodgenil ricxvebs shoris");
+                }
+            }
+        }
+    }
+}
